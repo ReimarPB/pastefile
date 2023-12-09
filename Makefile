1abfe3ac67c41dd70989ec60346181af454507d6
Reimar <mail@reim.ar>
2023-12-09 15:03:31 +0100

    Implement pasting images

--- Makefile
+++ Makefile
@@ -1,4 +1,3 @@
 all:
 	$(CC) -shared -o pastefile.so -fPIC pastefile.c -lprofanity -lX11
-	cp pastefile.so /home/reimar/pastefile.so
 
--- pastefile.c
+++ pastefile.c
@@ -6,13 +6,15 @@
 
 Display *display;
 Window window;
-Atom clipboard, targets, incr_type, output_atom;
+Atom clipboard, targets, incr_type, output_atom, png_target;
 int event_base, error_base;
 
-void pastefile(char **args)
+unsigned char *get_clipboard(Atom target, unsigned long *data_count)
 {
+	unsigned long data_size;
+
 	// Request selection
-	XConvertSelection(display, clipboard, targets, output_atom, window, CurrentTime);
+	XConvertSelection(display, clipboard, target, output_atom, window, CurrentTime);
 	XFlush(display);
 
 	// Wait for selection event
@@ -23,43 +25,67 @@ void pastefile(char **args)
 
 	if (event.xselection.property == None) {
 		prof_cons_show("No clipboard data found");
-		return;
+		return NULL;
 	}
-
-	// Get list of targets
 
 	Atom type;
 	int format;
-	unsigned long target_count, target_data_size;
-	unsigned char *target_data;
+	unsigned char *data;
 
 	// Get size of data
-	XGetWindowProperty(display, window, output_atom, 0, 0, False, AnyPropertyType, &type, &format, &target_count, &target_data_size, &target_data);
-	XFree(target_data);
+	XGetWindowProperty(display, window, output_atom, 0, 0, False, AnyPropertyType, &type, &format, data_count, &data_size, &data);
+	XFree(data);
 
+	// TODO implement
 	if (type == incr_type) {
 		prof_cons_show("INCR type not implemented");
-		return;
+		return NULL;
 	}
 
 	// Get actual data
-	XGetWindowProperty(display, window, output_atom, 0, target_data_size, False, AnyPropertyType, &type, &format, &target_count, &target_data_size, &target_data);
+	XGetWindowProperty(display, window, output_atom, 0, data_size, False, AnyPropertyType, &type, &format, data_count, &data_size, &data);
 	XDeleteProperty(display, window, output_atom);
 
-	Atom *target_atoms = (Atom *)target_data;
-
-	for (int i = 0; i < target_count; i++) {
-		char *target_name = XGetAtomName(display, target_atoms[i]);
-
-		char *msg = malloc(69);
-		snprintf(msg, 69, "Target: %s", target_name);
-		prof_cons_show(msg);
-		free(msg);
-
-		XFree(target_name);
+	if (data == NULL) {
+		prof_cons_show("Clipboard empty");
 	}
 
-	XFree(target_data);
+	return data;
+}
+
+void pastefile(char **args)
+{
+	unsigned long data_count;
+
+	// Get list of targets
+	Atom *target_atoms = (Atom *)get_clipboard(targets, &data_count);
+	if (target_atoms == NULL) return;
+
+	bool is_image = false;
+	for (int i = 0; i < data_count; i++) {
+		if (target_atoms[i] == png_target) is_image = true;
+	}
+
+	XFree(target_atoms);
+
+	// Get clipboard data
+	if (is_image) {
+		unsigned char *data = get_clipboard(png_target, &data_count);
+		if (data == NULL) return;
+
+		// Write to file
+		FILE *file = fopen("/tmp/image.png", "w");
+		if (file == NULL) {
+			prof_cons_show("Could not open file for writing");
+			return;
+		}
+		fwrite(data, data_count, 1, file);
+		fclose(file);
+
+		prof_send_line("/sendfile /tmp/image.png");
+	} else {
+		prof_cons_show("TODO text");
+	}
 }
 
 void prof_init(const char *version, const char *status, const char *account_name, const char *fulljid)
@@ -69,19 +95,19 @@ void prof_init(const char *version, const char *status, const char *account_name
 	clipboard = XInternAtom(display, "CLIPBOARD", False);
 	targets = XInternAtom(display, "TARGETS", False);
 	incr_type = XInternAtom(display, "INCR", False);
-	output_atom = XInternAtom(display, "XCLIP_OUT", False);
+	output_atom = XInternAtom(display, "CLIPBOARD_OUTPUT", False);
+	png_target = XInternAtom(display, "image/png", False);
 
 	// Create dummy window for receiving events
 	window = XCreateSimpleWindow(display, DefaultRootWindow(display), 0, 0, 1, 1, 0, 0, 0);
 	XSelectInput(display, window, PropertyChangeMask);
 
+	// Register /pastefile command
 	char *synopsis[] = { "/pastefile", NULL };
 	char *description = "Uploads the file currently stored on the clipboard";
 	char *arguments[][2] = { {NULL, NULL} };
 	char *examples[] = { NULL };
 	prof_register_command("/pastefile", 0, 0, synopsis, description, arguments, examples, pastefile);
-
-	prof_cons_show("Loaded pastefile plugin");
 }
 
 void prof_on_unload(void)
